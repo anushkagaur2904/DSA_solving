f2c65c8518c50ae772eb7299601b6a9fde2a7cce
Anushka Gaur <anushkagaur52@gmail.com>
2026-02-06 15:45:59 +0000

    check if array is sorted

---
+++ Arrays/Easy/03.cpp
@@ -0,0 +1,20 @@
+//https://www.geeksforgeeks.org/problems/check-if-an-array-is-sorted0701/1
+//check if array is sorted
+/*
+class Solution {
+  public:
+    bool isSorted(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        for(int i=1;i<n;i++){
+            if(arr[i]>=arr[i-1]){
+                
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
+*/
