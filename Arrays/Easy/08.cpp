58281515a82739cd13b0ceff69920defc0e592c6
Anushka Gaur <anushkagaur52@gmail.com>
2026-02-07 23:27:03 +0530

    linear search

---
+++ Arrays/Easy/08.cpp
@@ -0,0 +1,13 @@
+//linear search
+/*
+arr[] = [6,7,8,4,1] num =4
+tell first occurence of num
+for(i=0 to n){
+    if(arr[i]==num){
+        return i;
+    }
+    else{
+        return -1;
+    }
+}
+*/
