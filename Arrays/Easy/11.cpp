8a498e824d1097d48c387a4ea113642dfe824e30
Anushka Gaur <anushkagaur52@gmail.com>
2026-02-12 19:33:06 +0530

    second smallest

---
+++ Arrays/Easy/11.cpp
@@ -0,0 +1,15 @@
+//second smallest element in an array 
+/*
+int smallest = arr[0]
+int ssmallest = INT_MAX
+for(int i=0;i<n;i++){
+    if(arr[i]<smallest){
+        ssmallest=smallest
+        smallest=arr[i]
+    }
+    else if(arr[i]!=smallest && arr[i]<ssmallest){
+        ssmallest=arr[i]
+    }
+}
+
+*/
