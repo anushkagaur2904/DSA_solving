1113fe6931523c29381a1e4602ffe4d405d0ac5a
Anushka Gaur <anushkagaur52@gmail.com>
2026-03-03 21:48:55 +0530

    Search Insert Position

---
+++ Binary Search/1D Arrays/04.cpp
@@ -0,0 +1,14 @@
+//SEARCH INSERT POSITION
+//https://leetcode.com/problems/search-insert-position/
+/* given sorted array of distinct values and u have to find target 
+if target present then return index
+vrna btao ki sorted array mein kaha hota
+
+eg arr[] = [1,2,4,7] x=6
+index = 3 where 6 will be inserted
+
+THIS IS A CASE OF LOWER BOUND
+
+if x=2 insert wherever first 2 is
+basically return index where arr[index]>=x
+*/
