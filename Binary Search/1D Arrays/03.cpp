931d8d8ed28c955510177f2bea0538c08b685746
Anushka Gaur <anushkagaur52@gmail.com>
2026-03-03 21:41:37 +0530

    Upper Bound

---
+++ Binary Search/1D Arrays/03.cpp
@@ -0,0 +1,12 @@
+//UPPER BOUND
+//https://www.geeksforgeeks.org/problems/implement-upper-bound/1
+//smallest index such that arr[index]>x
+/*
+if(arr[mid]>x){
+    ans=mid;
+    high=mid-1;
+}
+else{
+    low=mid+1;
+}
+*/
