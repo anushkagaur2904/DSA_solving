ba152245499f2592b2568d25913020dd961e46c6
Anushka Gaur <anushkagaur52@gmail.com>
2026-03-03 21:17:36 +0530

    Search X in sorted array

--- Binary Search/1D Arrays/01.cpp
+++ Binary Search/1D Arrays/01.cpp
@@ -1,4 +1,5 @@
 //Binary Search
+//https://leetcode.com/problems/binary-search/
 //for sorted arrays and unique arrays i.e no duplicates
 //Real life example -> Dictionary
 
@@ -24,3 +25,47 @@ f(arr,n,target){
 */
 
 //+++++RECURSIVE CODE++++++++
+/*
+f(arr,low,high,target){
+    if(low>high){
+        return -1;
+    }
+    mid=(low+high)/2;
+    if(arr[mid]==target){
+        return mid;
+    }
+    else if(target>arr[mid]){
+        return f(arr,mid+1,high,target);
+    }
+    else{
+        return f(arr,low,mid-1,target);
+    }
+}
+*/
+
+/*
+TC => If initial size =32
+then after step 1 size might be <=16
+step 3 <=8
+step 4 <=4
+step 5 <=2
+step 6 <=1
+32 is 2 to the power of 5 
+total of steps = 6
+O(log n base 2)
+*/
+
+//+++++++++++OVERFLOW CLASE++++++++++++++++
+/*
+consider search space where low =0 and high = INT_MAX 
+i.e it is not an array
+if both low and high at INT_MAX
+then mid = (INT_MAX + INT_MAX)/2
+then it is overflow
+
+solution : assign long long to low and high
+
+or mid = low + (high-low)/2
+    i.e low + (INT_MAX - INT_MAX)/2
+and u can take both low and high as int
+*/
