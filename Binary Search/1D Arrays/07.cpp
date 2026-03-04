56ba370aeeb0dc62588554d3ea9605e269d4ce8e
Anushka Gaur <anushkagaur52@gmail.com>
2026-03-04 21:34:57 +0530

    COUNT OCCURRENCES OF A NUMBER IN A SORTED ARRAY WITH DUPLICATES

---
+++ Binary Search/1D Arrays/07.cpp
@@ -0,0 +1,4 @@
+//COUNT OCCURRENCES OF A NUMBER IN A SORTED ARRAY WITH DUPLICATES
+//https://www.geeksforgeeks.org/problems/number-of-occurrence2259/1
+//HINT: kahi bhi sorted array dikhe toh binary search
+//ANS: last - first +1
