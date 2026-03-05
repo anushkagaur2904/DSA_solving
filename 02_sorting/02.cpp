58424ef38c29ad279c77deb90e436c8d79be562e
Anushka Gaur <anushkagaur52@gmail.com>
2026-03-05 19:51:17 +0530

    BUBBLE SORT

---
+++ 02_sorting/02.cpp
@@ -0,0 +1,36 @@
+//BUBBLE SORT
+//push the max to last by adjacent swap
+/*
+step 1 -> 0 to n-1
+step 2 -> 0 to n-2
+last -> 0 to 1
+
+for(i=n-1;i>=1;i--){
+    for(j=0;j<i;j++){
+        if(arr[j]>arr[j+1]){
+            swap
+        }
+    }
+}
+
+TC => O(n^2) => worst case (avg case)
+*/
+
+//++++OPTIMIZE++++++++
+/*
+consider array 2 3 5 15 20
+check once 
+if 0 swap then break
+for(i=n-1;i>=1;i--){
+    int didSwap =0
+    for(j=0;j<i;j++){
+        if(arr[j]>arr[j+1]){
+            swap
+            didSwap=1
+        }
+    }
+    if(didSwap==0) break;
+}
+TC = > O(N) for best case
+*/
+
