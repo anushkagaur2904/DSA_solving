c031da9e5d8ba69cb43ad8b8d3f91fa7ca6f0c22
Anushka Gaur <anushkagaur52@gmail.com>
2026-03-07 21:30:35 +0530

    POTD

---
+++ POTD/03.cpp
@@ -0,0 +1,11 @@
+//6 MARCH 2026
+//https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/
+
+/*
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+         return s.find("01") == -1;
+    }
+};
+*/
