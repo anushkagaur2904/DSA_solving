2e12bd1a55f2c0bde6d533c35ef1c38990cc2ce5
Anushka Gaur <anushkagaur52@gmail.com>
2026-03-09 22:36:47 +0530

    new folder

---
+++ 04_Binary Search/1D Arrays/10.cpp
@@ -0,0 +1,25 @@
+//MINIMUM IN ROTATED SORTED ARRAY WITH UNIQUE ELEMENTS
+
+/*
+BRUTE FORCE
+TC => O(N)
+find minimum in array
+*/
+
+/*
+SORTED HAI TOH BINARY SEARCH
+identify the sorted half
+
+arr[] = [4,5,6,7,0,1,2]
+left half is sorted
+mtlb min part toh right part mein ho skta hai cuz vo rotating part hai
+
+arr[] = [7,8,1,2,3,4,5,6]
+right half sorted hai
+mtlb left half mein
+
+arr[] = [4,5,1,2,3]
+pr idhr right half sorted hai aur min bhi hai
+
+
+*/
