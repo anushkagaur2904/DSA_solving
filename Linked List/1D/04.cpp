d5ffa0b33574380fc9928080a46a6ad66ef44311
Anushka Gaur <anushkagaur52@gmail.com>
2026-03-16 19:54:22 +0530

    Length of LL

---
+++ Linked List/1D/04.cpp
@@ -0,0 +1,12 @@
+//Length of LL
+//https://www.geeksforgeeks.org/problems/count-nodes-of-linked-list/1
+//Never tamper with head pointer of LL, as it will be lost and you won't be able to traverse the LL again. Always use a temp pointer to traverse the LL.
+/*
+int cnt =0;
+Node* temp=head;
+while(temp!=NULL){
+    cnt++;
+    temp=temp->next;
+}
+return cnt;
+*/
