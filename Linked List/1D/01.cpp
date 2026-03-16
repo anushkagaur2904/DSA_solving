56b1fbec6780d5d6e87ee51b46e0697c2179ff7a
Anushka Gaur <anushkagaur52@gmail.com>
2026-03-16 18:20:25 +0530

    Intro To LL

--- Linked List/1D/01.cpp
+++ Linked List/1D/01.cpp
@@ -1,3 +1,4 @@
+//Intro To LL
 //arrays are stored in heap memory in contiguous location
 //LL help to increase and decrease the size
 //LL not in contiguous locations, but in heap
@@ -15,5 +16,55 @@ cout<<y
 struct node{
     int data
     node* next
+    Node(data1,next1){//creating constructor
+        data=data1
+        next=next1
+    }
 }
+*/
+
+//INITIALIZING VALUE
+/*
+Suppose i have to add 2
+Node x = Node(2,NULL) //data is 2 and next is NULL
+//this is just an object, not a pointer
+Node *y = &x //pointer to memory location of x
+//we can also do this in one line
+Node *y = new Node(2,NULL) //this will create a node in heap memory
+//this will store pointer to memory location of node in y
+
+struct Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+};
+int main(){
+    vector<int> arr = {2,5,8,7};
+    Node* y = new Node(arr[0],nullptr);
+    cout<<y//Memory location printed
+
+    Node y = node(arr[0],nullptr);//object created in stack memory
+    cout<<y.data//2 printed
+
+    Node *y = new Node(arr[0],nullptr);//object created in heap memory
+    cout<<y->data//2 printed
+}
+
+Disadvantages of using struct => cant use OOPS concept
+so use class instead of struct
+*/
+/*
+Memory space depends on system
+in 32 bit system -> 4 bytes for int and 4 bytes for pointer = 8 bytes
+in 64 bit system -> 4 bytes for int and 8 bytes for pointer = 12 bytes
+*/
+
+/*
+Difference between node and node* -> node is an object and node* is a pointer to an object
 */
