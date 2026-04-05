aa4a61b674fadca87ccfacdc562a4b80d40acc8a
Anushka Gaur <anushkagaur52@gmail.com>
2026-04-05 15:01:40 +0530

    stack

--- 09_Stack and Queue/Learn/02.cpp
+++ 09_Stack and Queue/Learn/02.cpp
@@ -1,4 +1,5 @@
 //STACK USING ARRAYS
+//https://www.geeksforgeeks.org/problems/implement-stack-using-array/1
 //not dynamic in nature
 /*
 size has to be known
@@ -40,3 +41,62 @@ SC => depends on size of array
       
 */
 
+/*
+class myStack {
+    int top;
+    int capacity;
+    int *st;
+  public:
+    myStack(int n) {
+        // Define Data Structures
+        capacity=n;
+        top=-1;
+        st = new int[n];
+    }
+
+    bool isEmpty() {
+        // check if the stack is empty
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    bool isFull() {
+        // check if the stack is full
+        if(top==capacity-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    void push(int x) {
+        // inserts x at the top of the stack
+        if(isFull()){
+            return;
+        }
+        top=top+1;
+        st[top]=x;
+    }
+
+    void pop() {
+        // removes an element from the top of the stack
+        if(isEmpty()){
+            return;
+        }
+        top=top-1;
+    }
+
+    int peek() {
+        // Returns the top element of the stack
+        if(isEmpty()){
+            return -1;
+        }
+        return st[top];
+    }
+};
+*/
