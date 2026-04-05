0083458a8d9b9ea123549a2ee03470e5d4e28603
Anushka Gaur <anushkagaur52@gmail.com>
2026-04-05 14:18:55 +0530

    implement using inbuilt libraries

---
+++ 09_Stack and Queue/Learn/01.cpp
@@ -0,0 +1,18 @@
+//STACK FOLLOWS LIFO
+//QUEUE FOLLOWS FIFO
+
+//++++++IMPLEMENT USING IN BUILT LIBRARY++++++++
+/*
+stack<int> st;
+st.push(6)
+st.pop()
+st.top()
+st.size()
+
+queue<int> q;
+q.push(3)
+q.pop()
+q.top()
+q.size()
+*/
+
