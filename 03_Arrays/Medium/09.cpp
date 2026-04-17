b6bc777f03e27fdd2f2752bbabdcb018c0d0c04d
Anushka Gaur <anushkagaur52@gmail.com>
2026-04-17 22:37:21 +0530

    Leaders in an array

--- 03_Arrays/Medium/09.cpp
+++ 03_Arrays/Medium/09.cpp
@@ -1,5 +1,5 @@
 //Leaders in an array
-
+//https://www.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1
 //+++++++++++BRUTE++++++++++
 /*
 vector<int> ans;
@@ -21,5 +21,22 @@ SC => O(n) at worst case to store answer
 
 //++++++++++++OPTIMAL++++++++++++
 /*
-
+class Solution {
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        // code here
+        int maxi = INT_MIN;
+        int n = arr.size();
+        vector<int> ans;
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]>=maxi){
+                ans.push_back(arr[i]);
+                maxi=arr[i];
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+SC => O(n) at worst case
 */
