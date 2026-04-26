85cef8c7bbeb73691a1a23d71eeaf4fe20a82073
Anushka Gaur <anushkagaur52@gmail.com>
2026-04-26 18:39:22 +0530

    max and min element in array

---
+++ 03_Arrays/Ques/02.cpp
@@ -0,0 +1,46 @@
+//Largest element in array
+//https://www.geeksforgeeks.org/problems/largest-element-in-array4009/1
+/*
+
+
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+        int largest = arr[0];
+        int n= arr.size();
+        for(int i=1;i<n;i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        return largest;
+        
+    }
+};
+//TC => O(N)
+
+/*
+BRUTE FORCE
+Sort then print arr[n-1]
+TC => O(NlogN)
+SC => O(1)
+*/
+
+//Smallest element in array
+/*
+int smallestElement(vector<int> &arr) {
+    int minElement = arr[0];  // assume first element is smallest
+
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] < minElement) {
+            minElement = arr[i];
+        }
+    }
+
+    return minElement;
+}
+Complexity
+Time: O(n)
+Space: O(1)
+*/
